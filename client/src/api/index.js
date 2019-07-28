9d3f2aca586f35e3d5348e66041e6ab0750dcc84
David <davidola360@gmail.com>
2019-07-28 19:07:25 +0100

    started mobile optimization and added heroku db

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,6 @@
 import Axios from 'axios';
-const baseURL = "http://Localhost:3030";
+// const baseURL = "http://Localhost:3030";
+const baseURL = "https://sela-assessment.herokuapp.com/"
 
 
 const axios = Axios.create({
